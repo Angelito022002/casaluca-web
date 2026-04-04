9ac5c57859aeba96c85a05d0a5239102a0ff8d3c
Angelito022002 <165395950+Angelito022002@users.noreply.github.com>
2026-04-04 15:24:37 -0400

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Casa Luca",
+  description: "Fine Dining Experience",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
